9f6c265c85ec3bd3aaef78ca5c189c280b95b4e8
Wes Gilleland <wes.gilleland@gmail.com>
2013-11-15 13:37:27 -0500

    Done for class. Need to do xn calculations

--- lab13/src/Runner.java
+++ lab13/src/Runner.java
@@ -1,20 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author Deathnerd
+ * class: CSC190
+ * project: lab13
+ * date: 11/15/2013
+ * author: Wes Gilleland
+ * purpose: This program will find the square root of a given number
  */
-public class Runner {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import java.util.Scanner;
+import java.lang.Math;
+public class Runner 
+{
+    static double getSqrt(double s){
+        double a = s;
+        int n = 0;
+        int y = 0;
+        int seed = 0;
+        
+        if (s == 0)
+            return 0;
+        else if (s >= 100){
+                //divide
+                while(a>=100){
+                        a /= 100;
+                        n++;
+                }
+        }
+        else {
+                //multiply
+                while (a < 1){
+                        a*=100;
+                        n--;
+                }
+        }
+        
+        //2*10^n
+        if (a < 10){
+            int k = 10;
+            for (int i = 0; i<=n; i++){
+                k*=10;
+            }
+            seed = 2*k;
+        }
+        else if (a >= 10){
+            int k = 10;
+            for (int i = 0; i<=n; i++){
+                k*=10;
+            }
+            seed = 6*k;
+        }
+        
+        //xn+1 = 1/2(xn+(s/xn))
+        double diff = .0000000001;
+        double x = .5*(seed+(s/seed)); //x0 calculation
+        double 
+        
+        //x1+ calculations
+        while (){
+        }
+        
+        System.out.println("N= "+n);
+        System.out.println("A= "+a);
+        System.out.println("Seed = "+seed);
+        return 0;
+    }
+    public static void main(String[] args) 
+    {
+        getSqrt(9);
     }
     
 }
