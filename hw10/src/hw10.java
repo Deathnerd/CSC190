8fd457a05d0bb9b19cce6bc56d1e4d818b0cdb22
Wes Gilleland <wes.gilleland@gmail.com>
2013-11-19 12:42:35 -0500

    Started hw10

---
+++ hw10/src/hw10.java
@@ -0,0 +1,17 @@
+/**
+ * class: CSC190
+ * project: hw10
+ * date: 11/19/13
+ * author: Wes Gilleland
+ * purpose: This program will take a string and compress the string, 
+ * then decompress the compressed string
+ */
+import java.util.Scanner;
+public class hw10 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
